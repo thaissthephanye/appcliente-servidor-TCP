e1ec7190c8829929a8d21505198fb29cc5c98987
Thais Sthephanye <thaissthephanye@gmail.com>
2021-12-16 13:42:12 -0200

    cliente

---
+++ cliente.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = 'localhost'
+PORT = 50000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall(str.encode(input('Digite algo: ')))
+data = s.recv(1024)
+
+print('Mensagem:' , data.decode())
